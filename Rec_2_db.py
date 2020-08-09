a227bb46a9c9fbaaa4d628c24e0685fb2d491049
Sundar Santhanam G <31473503+sundar19@users.noreply.github.com>
2020-08-09 19:01:08 +0530

    Add files via upload

---
+++ Rec_2_db.py
@@ -0,0 +1,53 @@
+import pyaudio
+import numpy as np
+import audioop
+import math
+import itertools
+import os
+
+CHUNK = 1024
+WIDTH = 2
+CHANNELS = 2
+FORMAT = pyaudio.paInt16
+RATE = 44100
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,#p.get_format_from_width(WIDTH)
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,#output = True
+                frames_per_buffer=CHUNK)
+
+db = 0
+frames = []
+buffer = []
+
+stream.start_stream()
+
+
+
+print("* recording")
+
+for x in itertools.repeat(1):
+    data = stream.read(CHUNK)
+    frames.append(data)
+    decoded = np.fromstring(data, 'Float32')
+    rms = audioop.rms(decoded, 2) # 2 bytes of audio data
+    print("rms:{}".format(rms))
+    try:
+        db = 20 * (math.log(rms,10))
+        print("db:{}".format(db))   
+    except:
+        pass
+    if (db > 74):
+        os.system('python D:/Python36/rec_2_detect.py')
+
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
+
+
+ 
---
+++ rec_2_detect.py
@@ -0,0 +1,93 @@
+import IPython
+import time
+import pickle
+import librosa
+import librosa.display
+import numpy as np
+import pyaudio
+import wave
+import torch
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 6
+WAVE_OUTPUT_FILENAME = "D:/Python36/Sound_datatest/file.wav"
+
+
+def spec_to_image(spec, eps=1e-6):
+    mean = spec.mean()
+    std = spec.std()
+    spec_norm = (spec - mean) / (std + eps)
+    spec_min, spec_max = spec_norm.min(), spec_norm.max()
+    spec_scaled = 255 * (spec_norm - spec_min) / (spec_max - spec_min)
+    spec_scaled = spec_scaled.astype(np.uint8)
+    return spec_scaled
+
+def get_melspectrogram_db(file_path, sr=None, n_fft=2048, hop_length=512, n_mels=128, fmin=20, fmax=8300, top_db=80):
+  wav,sr = librosa.load(file_path,sr=sr)
+  if wav.shape[0]<5*sr:
+    wav=np.pad(wav,int(np.ceil((5*sr-wav.shape[0])/2)),mode='reflect')
+  else:
+    wav=wav[:5*sr]
+  spec=librosa.feature.melspectrogram(wav, sr=sr, n_fft=n_fft,
+              hop_length=hop_length,n_mels=n_mels,fmin=fmin,fmax=fmax)
+  spec_db=librosa.power_to_db(spec,top_db=top_db)
+  return spec_db
+
+t1 = time.time()
+
+audio = pyaudio.PyAudio()
+
+# start Recording
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,
+                frames_per_buffer=CHUNK)
+print("recording...")
+stream.start_stream()
+frames = []
+ 
+# for recording 
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+print("finished recording")
+ 
+# stop Recording
+stream.stop_stream()
+
+while stream.is_active():
+    time.sleep(0.25)
+
+stream.close()
+audio.terminate()
+
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
+
+
+device=torch.device('cuda:0')
+
+
+
+with open('D:/Python36/model_import/indtocat.pkl','rb') as f:
+  indtocat = pickle.load(f)
+
+resnet_model = torch.load("D:/Python36/model_import/esc50resnet.pth")
+
+
+
+filename='D:/Python36/Sound_datatest/file.wav'
+spec=spec_to_image(get_melspectrogram_db(filename))
+spec_t=torch.tensor(spec).to(device, dtype=torch.float32)
+pr=resnet_model.forward(spec_t.reshape(1,1,*spec_t.shape))
+ind = pr.argmax(dim=1).cpu().detach().numpy().ravel()[0]
+t2=time.time()
+print("{} seconds".format(t2-t1))
+print(indtocat[ind])
+IPython.display.display(IPython.display.Audio(filename=filename))
